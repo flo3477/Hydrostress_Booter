971f54b922add7d52e96e80f096e2fc5fa6a5a81
flo3477 <eibiflo4@hotmail.com>
2017-01-26 17:21:33 +0100

    Update db.php

--- webgui_Free/includes/db.php
+++ webgui_Free/includes/db.php
@@ -5,6 +5,6 @@
 	define('DB_HOST', 'localhost');
 	define('DB_NAME', 'hackuniverse');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'hobbit36');
+	define('DB_PASSWORD', '');
 	$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
 ?>
